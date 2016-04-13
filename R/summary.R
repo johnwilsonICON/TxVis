cf836ef435102dff9a830e939e7a188d063188fa
johnwilsonICON <john.wilson@iconplc.com>
2016-04-13 12:44:30 -0700

    Amended file

---
+++ R/summary.R
@@ -0,0 +1,11 @@
+# summary.r
+#
+# This function will summarize useful information in a TxVis class object such as:
+#
+# 1. Number of: patients, treatments, & classes
+# 2. Mean, max and min: number of treatments/patient, start/end tx dates
+
+summary <- function() {
+  print("This function will summarize a TxVis class object!")
+}
+
