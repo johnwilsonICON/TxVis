408ba31e09319f6269ffde704c39e4f2ce36bd26
Simon <simon.j.goring@gmail.com>
2016-04-21 14:04:42 -0700

    Cleaned up the `create_txVis` function.  Created help for the `treat` object & added it into the `data` folder.
    **If the `treat` object needs to be updated, load it into memory from the csv file and then use `devtools::use_data(treat, overwrite = TRUE)`.**
    
    I added the ability to `roxygen`ize the help files automatically.  I'm working on minimally wrapping the functions so that the package actually loads.

--- R/create_txVis.R
+++ R/create_txVis.R
@@ -2,16 +2,26 @@
 #'
 #' The \code{txVis} object contains encoding for patient, treatment and dates, and can have
 #' encoding for additional discrete events.  The class is used throughout the package.
-#'
-#' @param x An object returned by one of the \code{get_*} commands for download, site or dataset.
-#' @param  ... other objects of the same class.
+#' @param patient Patient encoding for treatments.
+#' @param treatment Treatment encoding.
+#' @param start Start date for treatment in a format defined using \code{date_format}.
+#' @param end End date for treatment in a format defined using \code{date_format}.
+#' @param date_format Date format, as a character string.  See \code{as.Date}.
+#' @param ev.patient A patient encoding for each individual event.
+#' @param events Any point event in the treatment of each individual.
+#' @param event_date The date, formatted as \code{start} or \code{end}.
+#' 
 #' @author Ellen Korol \email{ellen.korol@@iconplc.com}
 #' @return This command returns an object of class \code{txVis}.
 #'
 #' @examples \dontrun{
 #'
-#'  x <- read.csv( "treat.csv" )
-#'  create_txVis( . . . )
+#'  data(treat)
+#'  hlth_data <- create_txVis(patient   = treat$patient, 
+#'                            treatment = treat$treatment,
+#'                            start     = treat$start,
+#'                            end       = treat$end,
+#'                            date_format = "%m %d, %Y")
 #'
 #' }
 #' @references
@@ -23,44 +33,63 @@
 # Notes:
 # object.name - the name 
 
-create_txVis <- function( object.name, data, patient, treatment, start, end, date_format, max_seq, events, event_date ) {
+create_txVis <- function(patient, 
+                         treatment, 
+                         start, 
+                         end, 
+                         date_format,
+                         ev_patient = NULL,
+                         events = NULL, 
+                         event_date = NULL) {
 
   # first element of list
-  patient <- as.character( patient ) # opinion on having this as character
-  treatment <- as.factor( treatment )
-  start <- as.Date( start, date_format)
-  end <- as.Date( end,date_format)
+  patient <- as.character(patient) # opinion on having this as character
+  treatment <- as.factor(treatment)
+  start <- as.Date(start, date_format)
+  end <- as.Date(end, date_format)
 
-  treats <- data.frame( pt_id = patient,
-                        tx = treatment,
-                        start_date = start,
-                        end_date = end , stringsAsFactors =F)
+  treats <- data.frame(pt_id            = patient,
+                       tx               = treatment,
+                       start_date       = start,
+                       end_date         = end,
+                       stringsAsFactors = FALSE)
   
-  events <- data.frame( pt_id = patient,
-                        event_date = event_date )
+  treats <- treats[order(start), ]
   
-  txVis <- list( treats, events )
+  if(is.null(ev_patient) | is.null(events)){
+    events <- data.frame()
+  } else {
+    
+    event_date <- as.Date(event_date, date_format)
+    
+    events <- data.frame(patient    = ev_patient,
+                         event      = events,
+                         event_date = event_date)
+    
+    events <- events[order(event_date), ]
+  }
+  
+  output <- list(treats, events)
 
-  class(name1) = c( 'txVis','data.frame')
+  class(output) = c('txVis','list')
 
-  name1 <- name1[with(name1, order(pt_id, start_date)), ]
+# SJG: I'm commenting this out.  Also, should we not be using only a single date?
+#      or might we expect that some events are multi-day?
+#  
+#   #second element of list
+#   #just doing this for now as a placeholder, but it will be the events data set
+# 
+#   name2 <- data.frame(pt_id = patient,
+#                    tx = treatment,
+#                    start_date = start,
+#                    end_date = end , stringsAsFactors = FALSE)
+# 
+#   class(name2) = c('data.frame')
+#  
+#  output <- list(txVis)
+#  
+#  class(output) = c('txVis', 'list')
 
-  #second element of list
-  #just doing this for now as a placeholder, but it will be the events data set
-  name2 <- data.frame(pt_id = patient,
-                     tx = treatment,
-                     start_date = start,
-                     end_date = end , stringsAsFactors =F)
-  class(name2) = c('data.frame')
-
-  #third element of list
-  name3 <- max_seq
-
-  name <- list (name1, name2, name3)
-  class(name) = c('txVis', 'list')
-
-  return(name)
-
+  return(output)
 
 }
-
