64cac487694dcca15a32dee3860041c0cbfba740
Korol <KorolE@eu.iconcr.com>
2016-05-12 13:40:02 -0700

    updates

--- R/create_txVis.R
+++ R/create_txVis.R
@@ -8,9 +8,11 @@
 #' @param start Start date for treatment in a format defined using \code{date_format}.
 #' @param end End date for treatment in a format defined using \code{date_format}.
 #' @param date_format Date format, as a character string.  See \code{as.Date}.
-#' @param ev.patient A patient encoding for each individual event.
+#' @param ev_patient A patient encoding for each individual event.
 #' @param events Any point event in the treatment of each individual.
-#' @param event_date The date, formatted as \code{start} or \code{end}.
+#' @param event_date The date of event, formatted as \code{date_format}.
+#' @param event_end_date The date that event finished, formatted as \code{date_format}.
+#' @param ev_format Date format for event dates, as a character string. See \code{as.Date}.
 #' 
 #' @author Ellen Korol \email{ellen.korol@@iconplc.com}
 #' @return This command returns an object of class \code{txVis}.
@@ -41,7 +43,9 @@ create_txVis <- function(patient,
                          date_format,
                          ev_patient = NULL,
                          events = NULL, 
-                         event_date = NULL) {
+                         event_date = NULL,
+                         event_end_date = NULL,
+                         ev_date_format = date_format) {
 
   # first element of list
   patient <- as.character(patient) # opinion on having this as character
@@ -56,28 +60,28 @@ create_txVis <- function(patient,
                        stringsAsFactors = FALSE)
   
   treats <- treats[order(start), ]
-  
+
+  # second element of list - default to NULL  
   if (is.null(ev_patient) | is.null(events)) {
     events <- data.frame()
   } else {
     
-    event_date <- as.Date(event_date, date_format)
+    event_date <- as.Date(event_date, date_format) 
     
-    events <- data.frame(patient    = ev_patient,
-                         event      = events,
-                         event_date = event_date)
+    event_end_date <- as.Date(event_end_date, date_format)
+    
+    events <- data.frame(ev_pt_id    = ev_patient,
+                         event       = events,
+                         ev_date     = event_date,
+                         ev_end_date = event_end_date)
     
     events <- events[order(event_date), ]
   }
   
+  # create class object
   output <- list(treats, events)
 
-  class(output) = c('txVis','list')
-
-# SJG: I'm commenting this out.  Also, should we not be using only a single date?
-#      or might we expect that some events are multi-day?
-#  
-
+  class(output) = c('txVis','list')  # as soon as i get here the output changes to two
 
   return(output)
 
--- R/print.txVis.R
+++ R/print.txVis.R
@@ -7,12 +7,14 @@ print.txVis <- function(x, ...) {
   cat('*** txVis object ***\n\n')
   cat(paste0(nrow(x[[1]]), ' unique treatment/patient pairs.\n'))
   cat(paste0(length(unique(x[[1]]$pt_id)), ' unique patients.\n'))
-  cat(paste0(length(unique(x[[1]]$tx)), ' unique treatments.\n\n'))
-  cat(paste0("date range from ", min(x[[1]]$start_date)," to ", max(x[[1]]$start_date), '\n'))
+  cat(paste0(length(unique(x[[1]]$tx)), ' unique treatment types.\n\n'))
+  cat(paste0("Date range from ", min(x[[1]]$start_date)," to ", max(x[[1]]$start_date), '\n'))
   if (nrow(x[[2]]) == 0) {
     cat("No coded event data.\n\n")
   } else {
-    cat(paste0(nrow(x[[2]]), " coded events over the course of the study set.\n\n"))
+  cat(paste0(nrow(x[[2]]), " coded events over the course of the study set.\n\n"))
+  cat(paste0(length(unique(x[[2]]$ev_pt_id )), ' unique patients with events.\n'))
+  cat(paste0(length(unique(levels(x[[2]]$events ))), ' types of events.\n'))
   }
   
   print.data.frame(head(x[[1]]))
